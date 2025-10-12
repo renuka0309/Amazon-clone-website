55bff401ebf7b070d01809ac856cdd97e3601c62
renuka <renukaullegaddi03@gmail.com>
2025-10-12 10:57:38 +0530

    move addToCart into cart.js

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,2 +1,20 @@
 export const cart = [];
 //export allows variable to be accessed out of this file
+export function addToCart(productId){
+  let matchingItem;
+
+      cart.forEach((cartItem)=>{
+       if(productId === cartItem.productId){
+        matchingItem=cartItem;
+       }
+      }); 
+
+      if(matchingItem){
+        matchingItem.quantity += 1;
+      }else{
+          cart.push({
+        productId: productId,
+        quantity: 1
+      }); 
+      }
+}
