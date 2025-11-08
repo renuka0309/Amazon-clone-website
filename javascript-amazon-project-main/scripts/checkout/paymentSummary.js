9b1b67e9b414674afc17426e5e0b652fcece69d9
renuka <renukaullegaddi03@gmail.com>
2025-11-08 11:30:33 +0530

    changes made in checkout.js file

---
+++ javascript-amazon-project-main/scripts/checkout/paymentSummary.js
@@ -0,0 +1,24 @@
+import {cart} from '../../data/cart.js';
+import { getProduct } from '../../data/products.js';
+import {getDeliveryOption} from '../../data/deliveryOptions.js';
+
+export function renderPaymentSummary(){
+    let productPriceCents = 0;
+    let shippingPriceCents = 0;
+
+    cart.forEach((cartItem)=>{
+        const product = getProduct(cartItem.productId);
+        productPriceCents+=product.priceCents * cartItem.quantity;
+
+       const deliveryOption =getDeliveryOption(cartItem.deliveryOPtionId);
+      shippingPriceCents+= deliveryOption.priceCents
+    });
+    const totalBeforeTaxCents = productPriceCents + shippingPriceCents;
+    const taxCents = totalBeforeTaxCents * 0.1;
+    const totalCents = totalBeforeTaxCents + taxCents;
+
+    
+}
+
+
+
