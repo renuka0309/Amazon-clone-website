92f50be660cf50e5990a713e715ccf10218d21eb
renuka <renukaullegaddi03@gmail.com>
2025-11-11 18:01:04 +0530

    save data and generate html for payment summary

--- javascript-amazon-project-main/scripts/checkout/orderSummary.js
+++ javascript-amazon-project-main/scripts/checkout/orderSummary.js
@@ -3,9 +3,9 @@
 //renderOrderSummary();
 //renderPaymentSummary();
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
-import { deliveryOptions } from '../../data/deliveryOptions.js';
+import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
 import {cart, removeFromCart, updateDeliveryOption} from '../../data/cart.js';
-import {products} from '../../data/products.js';
+import {products, getProduct} from '../../data/products.js';
 import {fromatCurrency} from '../utils/money.js';
 
 
@@ -16,23 +16,11 @@ let cartSummaryHTML = '';
 cart.forEach((cartItem) => {
     const productId = cartItem.productId;
 
-    let matchingProduct;
-
-    products.forEach((product)=>{
-      if(product.id === productId){
-        matchingProduct = product;
-      }
-    });
+    const matchingProduct = getProduct(productId);
 
    const deliveryOptionId = cartItem.deliveryOptionId;
 
-   let deliveryOption;
-
-   deliveryOptions.forEach((option)=>{
-    if(option.id === deliveryOptionId){
-      deliveryOption = option;
-    }
-   });
+   const deliveryOption = getDeliveryOption(deliveryOptionId);
 
    const today = dayjs();
         const deliveryDate = today.add(
--- javascript-amazon-project-main/scripts/checkout/paymentSummary.js
+++ javascript-amazon-project-main/scripts/checkout/paymentSummary.js
@@ -1,6 +1,7 @@
 import {cart} from '../../data/cart.js';
 import { getProduct } from '../../data/products.js';
 import {getDeliveryOption} from '../../data/deliveryOptions.js';
+import {fromatCurrency} from '../utils/money.js'
 
 export function renderPaymentSummary(){
     let productPriceCents = 0;
@@ -18,7 +19,42 @@ export function renderPaymentSummary(){
     const taxCents = totalBeforeTaxCents * 0.1;
     const totalCents = totalBeforeTaxCents + taxCents;
 
-    
+    const paymentSummaryHTML = `
+       <div class="payment-summary-title">
+            Order Summary
+          </div>
+
+          <div class="payment-summary-row">
+            <div>Items (3):</div>
+            <div class="payment-summary-money">$${fromatCurrency(productPriceCents)}</div>
+          </div>
+
+          <div class="payment-summary-row">
+            <div>Shipping &amp; handling:</div>
+            <div class="payment-summary-money">$${fromatCurrency(shippingPriceCents)}</div>
+          </div>
+
+          <div class="payment-summary-row subtotal-row">
+            <div>Total before tax:</div>
+            <div class="payment-summary-money">$${fromatCurrency(totalBeforeTaxCents)}</div>
+          </div>
+
+          <div class="payment-summary-row">
+            <div>Estimated tax (10%):</div>
+            <div class="payment-summary-money">$${fromatCurrency(taxCents)}</div>
+          </div>
+
+          <div class="payment-summary-row total-row">
+            <div>Order total:</div>
+            <div class="payment-summary-money">$${fromatCurrency(totalCents)}</div>
+          </div>
+
+          <button class="place-order-button button-primary">
+            Place your order
+          </button>
+    `;
+    document.querySelector('.js-payment-summary')
+      .innerHTML = paymentSummaryHTML;
 }
 
 
