d033cf4faba9fb03ad3e09d3870a07227ab00b7e
renuka <renukaullegaddi03@gmail.com>
2025-11-12 19:34:39 +0530

    add jasmin  testing framework

--- javascript-amazon-project-main/tests/moneyTest.js
+++ javascript-amazon-project-main/tests/moneyTest.js
@@ -23,3 +23,4 @@ if(fromatCurrency(2000.5)==='20.01'){  //edge case
 //                    2] Atomatic (writing test case in terms of code)
 // Two test cases : 1] Basic 2]Edge
 //Group of related test case is called => test suite
+//Testing Framework = external library that helps us write tests easier
