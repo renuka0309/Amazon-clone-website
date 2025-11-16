295a24b04b3174c703939f63cfcf490bdeb9cc66
renuka <renukaullegaddi03@gmail.com>
2025-11-16 19:27:05 +0530

    convert products into classes

--- javascript-amazon-project-main/data/products.js
+++ javascript-amazon-project-main/data/products.js
@@ -1,3 +1,5 @@
+import { fromatCurrency } from "../scripts/utils/money.js";
+
 export function getProduct(productId){
     let matchingProduct;
 
@@ -10,8 +12,32 @@ export function getProduct(productId){
   return matchingProduct;
 }
 
+class Product{
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+
+  constructor(productDetails){
+    this.id=productDetails.id;
+    this.image=productDetails.image;
+    this.name=productDetails.name;
+    this.rating=productDetails.rating;
+    this.priceCents=productDetails.priceCents;
+  }
+
+  getStarsUrl(){
+   return `images/ratings/rating-${this.rating.stars * 10}.png`;
+  }
+
+  getPrice(){
+    return `$${fromatCurrency(this.priceCents)}`
+  }
+}
+
 export const products = [
-  {
+ {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
     name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
@@ -669,6 +695,8 @@ export const products = [
       "mens"
     ]
   }
-];
+].map((productDetails)=>{           //transforming each of regular objects
+ return new Product(productDetails); // into a class
+});
 
 
--- javascript-amazon-project-main/scripts/checkout/orderSummary.js
+++ javascript-amazon-project-main/scripts/checkout/orderSummary.js
@@ -48,7 +48,7 @@ cart.forEach((cartItem) => {
                 ${matchingProduct.name}
                 </div>
                 <div class="product-price">
-                  $${(matchingProduct.priceCents / 100).toFixed(2)}
+                  ${matchingProduct.getPrice()}
                 </div>
                 <div class="product-quantity
                 js-product-quantity-${matchingProduct.id}">
