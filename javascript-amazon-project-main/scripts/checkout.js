30e23a6fc03fda86da807150c29daf36bda86420
renuka <renukaullegaddi03@gmail.com>
2025-11-17 10:19:06 +0530

    Learn promises and convert code to promises.

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -1,10 +1,53 @@
 import { renderOrderSummary } from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
 import { loadProducts } from '../data/products.js';
+import { loadCart } from '../data/cart.js';
 //import '../data/cart-class.js';
 //import '../data/backend-practice.js';
-loadProducts(()=>{
+
+Promise.all([
+    new Promise((resolve)=>{
+    loadProducts(()=>{
+        resolve('value1');
+    });
+
+    }),
+    new Promise((resolve)=>{
+            loadCart(()=>{
+                resolve();
+            });     
+        })
+]).then((values)=>{
   renderOrderSummary();
   renderPaymentSummary();
 });
 
+/*new Promise((resolve)=>{
+    loadProducts(()=>{
+        resolve('value1');
+    });
+
+    }).then((value)=>{
+        return new Promise((resolve)=>{
+            loadCart(()=>{
+                resolve();
+            });     
+        });
+     
+    }).then(()=>{
+       renderOrderSummary();
+       renderPaymentSummary();
+    });
+    */
+/*
+loadProducts(()=>{
+  renderOrderSummary();
+  renderPaymentSummary();
+});
+*/
+
+
+/**
+ * Promise is asynchronous code.
+ * resolve lets us control when to go to the next step.
+ */
